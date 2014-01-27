80834edaba080ea26d1ebd232b9ed0eef9745c01
Synthmax <max@synthmax.dk>
2014-01-27 01:58:11 +0100

    rails version bumbed down to 2.0.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
-set :application, 'capistrano-rails'
+set :application, 'digiramp'
 set :deploy_user, 'deploy'
 
 #
 # setup repo details
 set :scm, :git
-set :repo_url, 'https://github.com/maxgronlund/capistrano-rails'
+set :repo_url, 'https://github.com/maxgronlund/digiramp'
 
 # setup rvm.
 set :rbenv_type, :system
-set :rbenv_ruby, '2.1.0'
+set :rbenv_ruby, '2.0.0-p353'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
