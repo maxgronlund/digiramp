23558ece4c8b97c6d4a18447188ff31d02fc1266
Synthmax <max@synthmax.dk>
2014-02-04 01:36:37 +0100

    -

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,6 @@
 class AboutController < ApplicationController
   def index
     @blog = Blog.about
+    @peter = @blog.blog_posts.where(identifier: 'Peter Rafelson').first_or_create(identifier: 'Peter Rafelson', title: 'Peter Rafelson')
   end
 end
