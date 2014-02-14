6e8d48937d7692b79d00f93fcb6a5ba6803d9728
Synthmax <max@synthmax.dk>
2014-02-14 23:37:41 +0100

    -

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :keep_releases, 5
 set :linked_files, %w{config/database.yml config/application.yml}
 
 # dirs we want symlinking to shared
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system tmp/uploads}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # what specs should be run before deployment is allowed to
 # continue, see lib/capistrano/tasks/run_tests.cap
