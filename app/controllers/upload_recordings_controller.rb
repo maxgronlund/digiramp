c1401d86e8ec73ce8ae658ff295e55ddf289f0f4
Synthmax <max@synthmax.dk>
2014-02-22 18:01:42 +0100

    -

--- app/controllers/upload_recordings_controller.rb
+++ app/controllers/upload_recordings_controller.rb
@@ -1,11 +1,16 @@
 class UploadRecordingsController < ApplicationController
   before_filter :there_is_access_to_the_account
+  before_filter :get_blog
   def new
-    @recording_blog   = Blog.add_recording
-    @new_recording   = BlogPost.where(identifier: 'New Recording', blog_id: @recording_blog.id).first_or_create(identifier: 'New Recording', blog_id: @recording_blog.id, title: 'New Recording', body: 'New recording')
+    
+    @new_recording   = BlogPost.where(identifier: 'New Recording', blog_id: @blog.id).first_or_create(identifier: 'New Recording', blog_id: @blog.id, title: 'New Recording', body: 'New recording')
     @recording = Recording.new
   end
 
   def edit
+  end
+  
+  def get_blog
+    @blog   = Blog.add_recording
   end
 end
