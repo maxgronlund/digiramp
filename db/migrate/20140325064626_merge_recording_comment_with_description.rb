671e9c70eb262b0635b8f812a3ca4db84418a219
Synthmax <max@synthmax.dk>
2014-03-25 22:00:41 +0100

    -

--- db/migrate/20140325064626_merge_recording_comment_with_description.rb
+++ db/migrate/20140325064626_merge_recording_comment_with_description.rb
@@ -2,7 +2,7 @@ class MergeRecordingCommentWithDescription < ActiveRecord::Migration
   def change
     
     Recording.all.each do |recording|
-      if recording.description.empty? && !recording.comment.empty?
+      if( (recording.description.to_s == '') && (!recording.comment.to_s == ''))
         recording.description = recording.comment
         recording.save!
       end
@@ -12,7 +12,7 @@ class MergeRecordingCommentWithDescription < ActiveRecord::Migration
     
     
     Recording.all.each do |recording|
-      if recording.artists.empty? && !recording.artist.empty?
+      if( (recording.artists.to_s == '') && (!recording.artist.to_s == ''))
         recording.artists = recording.artist
         recording.save!
       end
