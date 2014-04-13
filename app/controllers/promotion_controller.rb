6b817325cf963148a5ce7c02fdee10f73b573e1f
Synthmax <max@synthmax.dk>
2014-04-13 02:07:29 +0200

    -

--- app/controllers/promotion_controller.rb
+++ app/controllers/promotion_controller.rb
@@ -1,7 +1,7 @@
 class PromotionController < ApplicationController
   before_filter :there_is_access_to_the_account
   def index
-    @blog       = Blog.promotion
-    @promotion  = BlogPost.where(identifier: 'Promotion', blog_id: @blog.id).first_or_create(identifier: 'Promotion', blog_id: @blog.id, title: 'Promotion', body: 'Distribute content') 
+    #@blog       = Blog.promotion
+    #@promotion  = BlogPost.where(identifier: 'Promotion', blog_id: @blog.id).first_or_create(identifier: 'Promotion', blog_id: @blog.id, title: 'Promotion', body: 'Distribute content') 
   end
 end
