6da9c82c5971b33abd021ecd89aa2d1252c5dfd3
Synthmax <max@synthmax.dk>
2014-04-17 00:37:32 +0200

    ruby upgrade

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/maxgronlund/digiramp.git'
 
 # setup rvm.
 set :rbenv_type, :system
-set :rbenv_ruby, '2.0.0-p353'
+set :rbenv_ruby, '2.1.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
