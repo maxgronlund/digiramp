0aba0934c3b8d957613e8eea603f5a347525dac3
Synthmax <max@synthmax.dk>
2014-04-17 01:30:21 +0200

    -

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,10 +89,23 @@ namespace :deploy do
   before :deploy, "deploy:run_tests"
   # compile assets locally then rsync
   after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
-  # start up sidekiq
-  after :deploy, "deploy:restart_sidekiq"
-
   after :finishing, 'deploy:cleanup'
+
+  # remove the default nginx configuration as it will tend
+  # to conflict with our configs.
+  before 'deploy:setup_config', 'nginx:remove_default_vhost'
+
+  # reload nginx to it will pick up any modified vhosts from
+  # setup_config
+  after 'deploy:setup_config', 'nginx:reload'
+
+  # Restart monit so it will pick up any monit configurations
+  # we've added
+  after 'deploy:setup_config', 'monit:restart'
+
+  # As of Capistrano 3.1, the `deploy:restart` task is not called
+  # automatically.
+  after 'deploy:publishing', 'deploy:restart'
 end
 
 #RAILS_ENV=production bundle exec sidekiq
