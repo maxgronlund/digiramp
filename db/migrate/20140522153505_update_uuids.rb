e08d10bde9daf1936386429e2d8d62a80cec8277
Synthmax <max@synthmax.dk>
2014-05-22 22:08:34 +0200

    skip validations on save

--- db/migrate/20140522153505_update_uuids.rb
+++ db/migrate/20140522153505_update_uuids.rb
@@ -6,20 +6,20 @@ class UpdateUuids < ActiveRecord::Migration
     
     # force new uuids
     Recording.all.each do |recording|
-      recording.save!
+      recording.save!(:validate => false)
     end
     
     User.all.each do |user|
-      user.save!
+      user.save!(:validate => false)
     end
     
     CommonWork.all.each do |common_work|
-      common_work.save!
+      common_work.save!(:validate => false)
       
     end
     
     AccountUser.all.each do |account_user|
-      account_user.save!
+      account_user.save!(:validate => false)
     end
     
     
