88666017f4b08f54405082ca2d61b1b214589d77
Synthmax <max@synthmax.dk>
2014-07-21 22:28:03 +0200

    -

--- db/migrate/20140721185817_add_access_account_to_account_user.rb
+++ db/migrate/20140721185817_add_access_account_to_account_user.rb
@@ -17,9 +17,22 @@ class AddAccessAccountToAccountUser < ActiveRecord::Migration
     return true if account_user.read_crm
     return true if account_user.read_opportunity
     return true if account_user.read_client
+    return true if account_user.read_recording
+    return true if account_user.read_file
+    return true if account_user.read_legal_document
+    return true if account_user.read_financial_document
+    return true if account_user.read_common_work
+    return true if account_user.read_user
+    return true if account_user.read_catalog
+    return true if account_user.read_playlist
+    return true if account_user.read_artwork
+    return true if account_user.read_client
+    return true if account_user.read_file
     return false
   end
     
     
     
 end
+
+
