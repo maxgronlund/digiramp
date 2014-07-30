e1a0f4420d1c9ea81345225cff06073f46fe0a44
Synthmax <max@synthmax.dk>
2014-07-30 23:04:48 +0200

    invite to new account

--- app/mailers/opportunity_mailer.rb
+++ app/mailers/opportunity_mailer.rb
@@ -14,9 +14,8 @@ class OpportunityMailer < ActionMailer::Base
     @oppertunity            = BlogPost.cached_find( "INVITE TO OPPORTUNITY" , blog )
 
     @opportunity_link       = url_for( controller: 'opportunity/opportunities', action: 'show', id: opportunity.id)
-    @fotter_link            = url_for( controller: 'contacts', action: 'new')
     @invitation             = opportunity_invitation.body
-
+    @fotter_link            = url_for( controller: 'contacts', action: 'new')
     
 
     mail to: email, subject: opportunity_invitation.title
@@ -24,17 +23,18 @@ class OpportunityMailer < ActionMailer::Base
   
   def invite_to_account email, opportunity_invitation_id, user_id
     
-    puts '------------------------------------------------'
-    puts 'INVITE TO ACCOUNT'
-    puts '-------------------------------------------------'
+
     
     user                    = User.cached_find(user_id)
     opportunity_invitation  = OpportunityInvitation.cached_find(opportunity_invitation_id)
     opportunity             = opportunity_invitation.opportunity
-
+    blog                    = Blog.cached_find('Support')
+    @oppertunity            = BlogPost.cached_find( "INVITE TO OPPORTUNITY" , blog )
+    
     @opportunity_link       = url_for( controller: 'activate_account', action: 'edit', id: user.password_reset_token, opportunity_id: opportunity.id  )
     @invitation             = opportunity_invitation.body
-    #@footer                 = footer.body
+    @fotter_link            = url_for( controller: 'contacts', action: 'new')
+
     
 
     mail to: email, subject: opportunity_invitation.title
