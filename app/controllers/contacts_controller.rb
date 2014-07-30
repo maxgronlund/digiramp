6ef5840af1ec2838a84ea0ecc4482cc7f10a3c91
Synthmax <max@synthmax.dk>
2014-07-30 22:27:15 +0200

    contacts updated

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,9 +2,9 @@ class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
 
 
-  def index
-    @contacts = Contact.all
-  end
+  #def index
+  #  @contacts = Contact.all
+  #end
 
   # GET /contacts/1
   # GET /contacts/1.json
@@ -38,27 +38,27 @@ class ContactsController < ApplicationController
 
   # PATCH/PUT /contacts/1
   # PATCH/PUT /contacts/1.json
-  def update
-    respond_to do |format|
-      if @contact.update(contact_params)
-        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
-        format.json { render :show, status: :ok, location: @contact }
-      else
-        format.html { render :edit }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #def update
+  #  respond_to do |format|
+  #    if @contact.update(contact_params)
+  #      format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
+  #      format.json { render :show, status: :ok, location: @contact }
+  #    else
+  #      format.html { render :edit }
+  #      format.json { render json: @contact.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   # DELETE /contacts/1
   # DELETE /contacts/1.json
-  def destroy
-    @contact.destroy
-    respond_to do |format|
-      format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  #def destroy
+  #  @contact.destroy
+  #  respond_to do |format|
+  #    format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
+  #    format.json { head :no_content }
+  #  end
+  #end
 
   private
     # Use callbacks to share common setup or constraints between actions.
