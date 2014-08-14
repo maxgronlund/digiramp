69999c5c240b12b01dd4bbaf9f6cc42398d9c6b6
Abdelkader Boudih <terminale@gmail.com>
2014-08-14 22:24:37 +0000

    Update opportunity_invitation_worker.rb
    Raise error so sidekiq retry to resend the email.

--- app/workers/opportunity_invitation_worker.rb
+++ app/workers/opportunity_invitation_worker.rb
@@ -2,6 +2,6 @@ class OpportunityInvitationWorker
   include Sidekiq::Worker
   
   def perform(email, opportunity_invitation_id, user_id, current_user_id)
-    OpportunityMailer.invite(email, opportunity_invitation_id, user_id, current_user_id).deliver
+    OpportunityMailer.invite(email, opportunity_invitation_id, user_id, current_user_id).deliver!
   end
 end
