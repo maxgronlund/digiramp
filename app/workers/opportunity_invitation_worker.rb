1ef7c4a5a925b9723fc895ca57b706626d9eacc4
Abdelkader Boudih <terminale@gmail.com>
2014-08-14 22:21:38 +0000

    Update opportunity_invitation_worker.rb

--- app/workers/opportunity_invitation_worker.rb
+++ app/workers/opportunity_invitation_worker.rb
@@ -1,7 +1,7 @@
 class OpportunityInvitationWorker
   include Sidekiq::Worker
   
-  def perform
-    OpportunityMailer.invite()
+  def perform(email, opportunity_invitation_id, user_id, current_user_id)
+    OpportunityMailer.invite(email, opportunity_invitation_id, user_id, current_user_id).deliver
   end
 end
