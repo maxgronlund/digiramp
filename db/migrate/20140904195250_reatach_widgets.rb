e2a573d6277cf7ef937c5f011f87fc71985b5c93
Synthmax <max@synthmax.dk>
2014-09-06 08:48:26 +0200

    -

--- db/migrate/20140904195250_reatach_widgets.rb
+++ db/migrate/20140904195250_reatach_widgets.rb
@@ -1,35 +1,35 @@
 class ReatachWidgets < ActiveRecord::Migration
   def change
     
-    Playlist.destroy_all
-    Widget.all.each do |widget|
-      if widget.secret_key.to_s.size < 10
-        # get widgets connected to a catalog
-        if widget.catalog_id
-          # find catalog
-          if catalog = Catalog.where(id: widget.catalog_id.to_i).first
-
-            
-            # destroy catalog widget
-            widget.destroy
-            
-            # add recordings to playlist
-            catalog.add_recordings catalog.recordings
-            
-            # log resoult
-            ap catalog.default_widget
-            if catalog.default_widget.recordings
-              catalog.default_widget.recordings.each do |recording|
-                ap recording
-              end
-            end
-          end
-        else
-          puts '--------------------------- all ready a playlist --------------------------'
-        end
-        
-      end
-      
-    end
+    #Playlist.destroy_all
+    #Widget.all.each do |widget|
+    #  if widget.secret_key.to_s.size < 10
+    #    # get widgets connected to a catalog
+    #    if widget.catalog_id
+    #      # find catalog
+    #      if catalog = Catalog.where(id: widget.catalog_id.to_i).first
+    #
+    #        
+    #        # destroy catalog widget
+    #        widget.destroy
+    #        
+    #        # add recordings to playlist
+    #        catalog.add_recordings catalog.recordings
+    #        
+    #        # log resoult
+    #        ap catalog.default_widget
+    #        if catalog.default_widget.recordings
+    #          catalog.default_widget.recordings.each do |recording|
+    #            ap recording
+    #          end
+    #        end
+    #      end
+    #    else
+    #      puts '--------------------------- all ready a playlist --------------------------'
+    #    end
+    #    
+    #  end
+    #  
+    #end
   end
 end
