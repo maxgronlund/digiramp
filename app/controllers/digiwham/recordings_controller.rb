5683ee51eeb48f08ae7b6c02f06d2278e10eba6c
Synthmax <max@synthmax.dk>
2014-11-02 00:05:13 +0100

    -

--- app/controllers/digiwham/recordings_controller.rb
+++ app/controllers/digiwham/recordings_controller.rb
@@ -17,24 +17,26 @@ class Digiwham::RecordingsController < ApplicationController
   
   # the show function is used for count of playbacks
   def show
-    
-    @recording                  = Recording.cached_find(params[:id])
-    @recording.playbacks_count  += 1
-    @recording.uniq_playbacks_count = Uniqifyer.uniqify(@recording.playbacks_count)
-    @recording.save!
-    user_id                     = current_user ? current_user.id : nil
-    playback = Playback.create(
-                      recording_id: @recording.id, 
-                      user_id: user_id, 
-                      account_id: @recording.account_id 
-                    )
-
-    if current_user                
-      current_user.create_activity(  :created, 
-                                 owner: playback, # the recording has many comments
-                             recipient: @recording,
-                        recipient_type: 'Recording',
-                            account_id: @recording.user.account_id) 
+    begin
+      @recording                  = Recording.cached_find(params[:id])
+      @recording.playbacks_count  += 1
+      @recording.uniq_playbacks_count = Uniqifyer.uniqify(@recording.playbacks_count)
+      @recording.save!
+      user_id                     = current_user ? current_user.id : nil
+      playback = Playback.create(
+                        recording_id: @recording.id, 
+                        user_id: user_id, 
+                        account_id: @recording.account_id 
+                      )
+      
+      if current_user                
+        current_user.create_activity(  :created, 
+                                   owner: playback, # the recording has many comments
+                               recipient: @recording,
+                          recipient_type: 'Recording',
+                              account_id: @recording.user.account_id) 
+      end
+    rescue
     end
     #widget = Widget.cached_find(params[:widget_id])
     #widget.playback_count += 1
