0a3f5a92ee1e4f2dadcbb17c8d28d8d7be1173a8
Synthmax <max@synthmax.dk>
2014-11-20 02:19:20 +0100

    -

--- app/controllers/opportunity/music_submissions_controller.rb
+++ app/controllers/opportunity/music_submissions_controller.rb
@@ -17,7 +17,8 @@ class Opportunity::MusicSubmissionsController < ApplicationController
   def new
     @music_request   = MusicRequest.cached_find(params[:music_request_id])
     
-
+    opportunity   =  @music_request.opportunity
+    account_id    =  opportunity.account_id
     
     submitted_recording_ids = MusicSubmission.where(music_request_id: @music_request.id).pluck(:recording_id)
     
@@ -30,16 +31,26 @@ class Opportunity::MusicSubmissionsController < ApplicationController
     account_users =  AccountUser.where(user_id: current_user.id)
     
     #ap account_users.first
+    opportunity   =  @music_request.opportunity
+    account_id    =  opportunity.account_id
     
-    # !!! optimize
-    recording_ids = []
-    account_users.each do |account_user|
-      # add some permissions here
-      # if account user can submit to requests
-      recording_ids   += account_user.account.recording_ids
+    if account_id == current_user.account_id
+      # !!! optimize
+      recording_ids = []
+      account_users.each do |account_user|
+        # add some permissions here
+        # if account user can submit to requests
+        recording_ids   += account_user.account.recording_ids
+        
+      end
+      recording_ids   -= submitted_recording_ids
+    else  
+      # remove recordings not submitted by current user
       
+      current_users_submissions_recording_ids = MusicSubmission.where(user_id: current_user.id, music_request_id: @music_request.id).pluck(:recording_id)
+      recording_ids = current_users_submissions_recording_ids
     end
-    recording_ids   -= submitted_recording_ids
+    #
     @recordings =  Recording.not_in_bucket.search_from_ids(recording_ids, params[:query]).order('title asc').page(params[:page]).per(48)
     #end
     @user = current_user
