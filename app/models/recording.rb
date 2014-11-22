e9a2aa22ad0eef682df07b5e20efcf48d5b67811
Synthmax <max@synthmax.dk>
2014-11-22 12:28:44 +0100

    -

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -28,13 +28,32 @@ class Recording < ActiveRecord::Base
                                       :mood,
                                       :instruments,
                                       :tempo 
-                                    ], :using => {  :tsearch => { :prefix => true, :any_word => true, dictionary: "english"},
-                                                    :dmetaphone => {:any_word => true, :sort_only => true},
-                                                    :trigram => {:threshold => 0.1 }
-                                                  }
-                                                  
-                                        #,
-                                        #ignoring: :accents
+                                    ], :using => [:tsearch, :dmetaphone, :trigram]
+                                    
+                                    #pg_search_scope :search, against: [ :title, 
+                                    #                                    :lyrics, 
+                                    #                                    :genre, 
+                                    #                                    :artist, 
+                                    #                                    :bpm, 
+                                    #                                    :comment, 
+                                    #                                    :vocal, 
+                                    #                                    :isrc_code,
+                                    #                                    :copyright,
+                                    #                                    :production_company,
+                                    #                                    :upc_code,
+                                    #                                    :year,
+                                    #                                    :album_name,
+                                    #                                    :performer,
+                                    #                                    :band,
+                                    #                                    :mood,
+                                    #                                    :instruments,
+                                    #                                    :tempo 
+                                    #                                  ], :using => {  :tsearch => { :prefix => true, :any_word => true, dictionary: "english"},
+                                    #                                                  :dmetaphone => {:any_word => true, :sort_only => true},
+                                    #                                                  :trigram => {:threshold => 0.1 }
+                                    #                                                },
+                                    #                                      ignoring: :accents
+                                    #
   
   
   validates :title, :presence => true
