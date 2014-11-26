9b20eafd341360166968b8e0366667291a9a30bc
Synthmax <max@synthmax.dk>
2014-11-26 19:32:39 +0100

    -

--- app/controllers/recording_playbacks_controller.rb
+++ app/controllers/recording_playbacks_controller.rb
@@ -4,9 +4,11 @@ class RecordingPlaybacksController < ApplicationController
     @recording = Recording.cached_find(params[:id])
     @authorized = false
     
-    if (current_user && current_user.id == @user.id) 
-      @authorized = true
+    if current_user
+      @authorized = true if current_user.id == @user.id
+      @authorized = true if current_user.super?
     end
+
     ap @recording
   end
 end
