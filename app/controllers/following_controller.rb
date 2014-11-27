a8c97c28fc4625628c694f812b277af158e0a4d2
Synthmax <max@synthmax.dk>
2014-11-27 01:43:58 +0100

    -

--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -1,7 +1,7 @@
 class FollowingController < ApplicationController
-  #before_filter :get_user
+  before_filter :get_user
   
   def index
-    ap params
+    @followed_users = @user.followed_users
   end
 end
