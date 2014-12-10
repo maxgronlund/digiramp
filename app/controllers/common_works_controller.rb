2f71deed52010f0718f9c87ce82d1001d1deef3f
Synthmax <max@synthmax.dk>
2014-12-10 18:34:27 +0100

    -

--- app/controllers/common_works_controller.rb
+++ app/controllers/common_works_controller.rb
@@ -6,37 +6,43 @@ class CommonWorksController < ApplicationController
 
   
   def update
-
+    ap params[:common_work].nil? 
+    
     recording_id   = params[:common_work][:recording_id]
     user_id       = params[:common_work][:user_id]
     params[:common_work].delete :recording_id
     params[:common_work].delete :user_id
+    
+    
     #forbidden unless current_account_user.update_common_work
     #artwork_url = TransloaditImageParser.get_image_url params[:transloadit]
 
     # extract  parameters
-    params[:common_work] = params["common_work"]
+    params[:common_work] = params["common_work"] 
     
     # set the artwork url if any
     #params[:common_work][:artwork]  = artwork_url if artwork_url
 
     
     @common_work    = CommonWork.cached_find(params[:id])
-    if @common_work.update_attributes(common_work_params)
-      
-      @common_work.create_activity(  :updated, 
-                                owner: current_user,
-                            recipient: @common_work,
-                       recipient_type: @common_work.class.name,
-                           account_id: @common_work.account_id)
-                           
-      @common_work.update_completeness
-      @common_work.attache_ipis_true recording_id
-      redirect_to user_recording_path( user_id, recording_id)
-      
-      
+    
+    if  params[:common_work][:ipis_attributes]
+      if @common_work.update_attributes(common_work_params)
+        
+        @common_work.create_activity(  :updated, 
+                                  owner: current_user,
+                              recipient: @common_work,
+                         recipient_type: @common_work.class.name,
+                             account_id: @common_work.account_id)
+                             
+        @common_work.update_completeness
+        @common_work.attache_ipis_true recording_id
+        redirect_to user_recording_path( user_id, recording_id)
+      else
+        redirect_to :back
+      end
     else
-      redirect_to :back
+      redirect_to user_recording_path( user_id, recording_id)
     end
   end
   
