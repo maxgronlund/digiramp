1020ab851f6391ddd2e7a9c8e05f0333bd87b67f
Synthmax <max@synthmax.dk>
2014-12-21 21:17:04 +0100

    -

--- app/models/opportunities_users.rb
+++
@@ -1,4 +0,0 @@
-class OpportunitiesUsers < ActiveRecord::Base
-  belongs_to :opportunity
-  belongs_to :user
-end
--- config/rubyzip.rb
+++
@@ -1,7 +0,0 @@
-Zip.setup do |c|
-  c.on_exists_proc          = true
-  c.continue_on_exists_proc = true
-  c.unicode_names           = true
-  c.default_compression     = Zlib::DEFAULT_COMPRESSION
-  c.write_zip64_support     = true
-end
