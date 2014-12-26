e651c994f955ee02ff532719e17c22871defba23
Synthmax <max@synthmax.dk>
2014-12-26 18:02:32 +0100

    delete opportunities from admin

--- app/controllers/admin/opportunities_controller.rb
+++ app/controllers/admin/opportunities_controller.rb
@@ -1,5 +1,5 @@
 class Admin::OpportunitiesController < ApplicationController
-  before_action :set_opportunity, only: [:show, :edit, :update, :destroy, :music_submissions]
+  #before_action :set_opportunity, only: [:show, :edit, :update, :destroy, :music_submissions]
   
   before_filter :admin_only
 
@@ -8,6 +8,12 @@ class Admin::OpportunitiesController < ApplicationController
     @opportunities  = Opportunity.search(params[:query]).order('deadline desc').page(params[:page]).per(50)
     @user   = current_user
     @authorized = true
+  end
+  
+  def destroy
+    opportunity           = Opportunity.cached_find(params[:id])
+    @opportunity_id       = opportunity.id
+    opportunity.destroy
   end
 
 
