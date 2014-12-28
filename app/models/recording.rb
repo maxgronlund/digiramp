10d8e8e48c1b8514ae4a250e852708f0cfe78b27
Synthmax <max@synthmax.dk>
2014-12-28 01:01:07 +0100

    pg search updated

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -32,11 +32,12 @@ class Recording < ActiveRecord::Base
                                       :instruments,
                                       :tempo 
                                     ], 
-                            using: {  tsearch: { prefix: true, 
+                            using: {  dmetaphone: {:any_word => true, :sort_only => true},
+                                      tsearch: { prefix: true, 
                                                  any_word: true, 
                                                  dictionary: "english"
-                                                },
-                                      dmetaphone: {:any_word => true, :sort_only => true}
+                                                }
+                                      
                                     }
                                     #,
                             #ignoring: :accents
