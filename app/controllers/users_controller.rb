4a3d320a4efd33f28974761814acd12eb56312eb
Synthmax <max@synthmax.dk>
2014-12-30 23:02:42 +0100

    -

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,12 +67,16 @@ class UsersController < ApplicationController
   end
   
   def find_user
-    @user = User.cached_find(params[:id])
-    # If an old id or a numeric id was used to find the record, then
-    # the request path will not match the post_path, and we should do
-    # a 301 redirect that uses the current friendly id.
-    if request.path != user_path(@user)
-      return redirect_to @user, :status => :moved_permanently
+    begin
+      @user = User.cached_find(params[:id])
+      # If an old id or a numeric id was used to find the record, then
+      # the request path will not match the post_path, and we should do
+      # a 301 redirect that uses the current friendly id.
+      if request.path != user_path(@user)
+        return redirect_to @user, :status => :moved_permanently
+      end
+    rescue
+      not_found
     end
   end
   
