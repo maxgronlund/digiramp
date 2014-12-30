5d21c143beb3435a0a137b7a3df09e973a7f4608
Synthmax <max@synthmax.dk>
2014-12-30 23:49:43 +0100

    -

--- app/controllers/recording_likes_controller.rb
+++ app/controllers/recording_likes_controller.rb
@@ -2,16 +2,19 @@ class RecordingLikesController < ApplicationController
   before_filter :get_user
   
   def index
-    
-    @recording    = Recording.cached_find(params[:recording_id])
-    
-    # !!! optimization needed
-    # fetching all likes on each ajax request
-    user_ids      = Like.where(recording_id: @recording.id).pluck(:user_id)
-    @users        = User.where(id: user_ids).page(params[:page]).per(4)
-    
-    @show         = 'likes for a recording'
-    @playlists    = current_user.playlists if current_user
+    begin
+      @recording    = Recording.cached_find(params[:recording_id])
+      
+      # !!! optimization needed
+      # fetching all likes on each ajax request
+      user_ids      = Like.where(recording_id: @recording.id).pluck(:user_id)
+      @users        = User.where(id: user_ids).page(params[:page]).per(4)
+      
+      @show         = 'likes for a recording'
+      @playlists    = current_user.playlists if current_user
+    rescue
+      not_found
+    end
 
   end
 end
