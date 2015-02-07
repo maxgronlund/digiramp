750a09cac624ee6ada50be3df18555c02e355944
Synthmax <max@synthmax.dk>
2015-02-07 19:31:57 +0100

    -

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207145739) do
+ActiveRecord::Schema.define(version: 20150207182524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
