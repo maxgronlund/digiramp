cf69c581ccab54bfdec9e7527eeb23e7eaf9a092
Synthmax <max@synthmax.dk>
2015-02-07 20:01:25 +0100

    -

--- app/controllers/user/cms_module_moves_controller.rb
+++ app/controllers/user/cms_module_moves_controller.rb
@@ -5,6 +5,13 @@ class User::CmsModuleMovesController < ApplicationController
     @column_nr    = cms_section.column_nr
     @cms_page     = CmsPage.cached_find(params[:cms_page_id])
     
+    # make sure the sections is ordered propperly
+    @cms_page.cms_sections.order(:position).where(column_nr: cms_section.column_nr).each_with_index do |section, index|
+      section.position = index
+      section.save!
+    end
+    
+    # swap sections
     @cms_page.cms_sections.order(:position).where(column_nr: cms_section.column_nr).each do |section|
       
       if section.position == cms_section.position - 1
