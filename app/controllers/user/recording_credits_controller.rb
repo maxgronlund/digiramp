d830a76603b4389710eccf34c67d8c6edd2f8cbb
Synthmax <max@synthmax.dk>
2015-02-13 22:57:48 +0100

    -

--- app/controllers/user/recording_credits_controller.rb
+++ app/controllers/user/recording_credits_controller.rb
@@ -17,7 +17,7 @@ class User::RecordingCreditsController < ApplicationController
     @recording.update_attributes(recording_params) 
     #@recording.confirm_ipis
 
-    redirect_to :back
+    redirect_to user_user_creative_rights_path(@user)
     #
     #if params[:commit] == 'Save'
     #  redirect_to edit_user_recording_right_path(@recording.user, @recording)
