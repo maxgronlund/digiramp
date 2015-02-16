b6e95a30dfd1d457e59305a6bacafeb5a6c2932e
Synthmax <max@synthmax.dk>
2015-02-16 15:42:47 +0100

    -

--- app/controllers/user/recording_confirmations_controller.rb
+++ app/controllers/user/recording_confirmations_controller.rb
@@ -1,10 +1,10 @@
 class User::RecordingConfirmationsController < ApplicationController
   def update
-    case params[:commit]
-      
-    when
-      
-    end
+    #case params[:commit]
+    #  
+    #when
+    #  
+    #end
 
   end
 end
