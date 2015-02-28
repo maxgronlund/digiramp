a4c751640a26cbb1385e33d9468e61696ffbbba0
Synthmax <max@synthmax.dk>
2015-02-28 02:53:16 +0100

    -

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,9 @@ module ApplicationHelper
   
   def sortable(column, title = nil)
     title ||= column.titleize
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
-    link_to title, :sort => column, :direction => direction
+    link_to title, { sort: column, direction: direction}, class: css_class
   end
     
     
