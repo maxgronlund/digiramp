91e65e9b85223d9714e37490adb81832608ba204
Synthmax <max@synthmax.dk>
2015-02-28 01:56:38 +0100

    -

--- app/controllers/unread_messages_controller.rb
+++ app/controllers/unread_messages_controller.rb
@@ -1,5 +1,6 @@
 class UnreadMessagesController < ApplicationController
   before_filter :access_user, only: [:index]
   def index
+    @new_messages = @user.received_massages.where(read: false).order(created_at: :desc).page(params[:page]).per(24)
   end
 end
