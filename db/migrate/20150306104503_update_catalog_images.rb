c2e1ce695605c0ca1a0751566f432f6356a08c16
Synthmax <max@synthmax.dk>
2015-03-06 14:10:58 +0100

    -

--- db/migrate/20150306104503_update_catalog_images.rb
+++ db/migrate/20150306104503_update_catalog_images.rb
@@ -1,8 +1,8 @@
 class UpdateCatalogImages < ActiveRecord::Migration
   def change
     
-    Catalog.find_each do |catalog|
-      catalog.check_default_image
-    end
+    #Catalog.find_each do |catalog|
+    #  catalog.check_default_image
+    #end
   end
 end
