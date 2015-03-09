e7b58f002447542386a81d0aa22786b0d0520048
Synthmax <max@synthmax.dk>
2015-03-09 19:46:05 +0100

    -

--- app/controllers/log_in_or_signup_controller.rb
+++ app/controllers/log_in_or_signup_controller.rb
@@ -1,6 +1,5 @@
 class LogInOrSignupController < ApplicationController
   def new
-    ap params
     @message                = params[:message]
     session[:go_to_message] = params[:bounce_back]
   end
