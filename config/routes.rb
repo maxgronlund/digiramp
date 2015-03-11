57513928375f2f622f1f6f87663495d5d8bcb5c8
Synthmax <max@synthmax.dk>
2015-03-11 21:06:31 +0100

    -

--- config/routes.rb
+++ config/routes.rb
@@ -679,7 +679,7 @@ Digiramp::Application.routes.draw do
   namespace :user do
     #resources :common_work_lyrics
     resources :users do
-      resources :creative_rights, only: [:index]
+      resources :creative_rights, only: [:index, :show]
       resources :common_works do
         resources :common_work_contracts
       end
