78174130ca975d73300b1c63b32ba3de042282cc
Synthmax <max@synthmax.dk>
2015-03-15 15:57:50 +0100

    -

--- db/migrate/20150315125020_update_user_avatars.rb
+++ db/migrate/20150315125020_update_user_avatars.rb
@@ -2,11 +2,17 @@ class UpdateUserAvatars < ActiveRecord::Migration
   def change
     
     User.find_each do |user|
-      if File.exist?(Rails.root.join('public' +  user.image_url.to_s))
-        user.image.recreate_versions!(:avatar_220x220)
-        user.save!
-      else
-        user.set_default_avatar
+      begin
+        if File.exist?(Rails.root.join('public' +  user.image_url.to_s))
+          user.image.recreate_versions!(:avatar_220x220)
+          user.save!
+        else
+          user.set_default_avatar
+        end
+      rescue
+        puts '========================================================================================================================================='
+        puts user.id
+        puts '========================================================================================================================================='
       end
      
     end
