f010a5889bff6eb254a99e0d05e432b5828debb7
Synthmax <max@synthmax.dk>
2015-03-25 09:47:54 +0100

    deleted removed resources

--- app/controllers/xadd_catalog_assets_controller.rb
+++
@@ -1,7 +0,0 @@
-class AddCatalogAssetsController < ApplicationController
-  include AccountsHelper
-  before_filter :access_account
-  def show
-     @catalog = Catalog.cached_find(params[:id])
-  end
-end
--- app/controllers/xadd_content_controller.rb
+++
@@ -1,8 +0,0 @@
-class AddContentController < ApplicationController
-  include AccountsHelper
-  before_filter :access_account
-  
-  def index
-    forbidden unless current_account_user.can_add_content?
-  end
-end
