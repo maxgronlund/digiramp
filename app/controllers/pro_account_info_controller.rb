c78cc6c857170e6ad0d9c2154c9ee73cd45b97ce
Synthmax <max@synthmax.dk>
2015-03-25 13:07:33 +0100

    -

--- app/controllers/pro_account_info_controller.rb
+++ app/controllers/pro_account_info_controller.rb
@@ -1,19 +1,27 @@
 class ProAccountInfoController < ApplicationController
   def index
-
+    session[:return_url] = request.url
     blog = Blog.where(identifier: 'accounts info')
                 .first_or_create(identifier: 'accounts info', title: 'Accounts Info')
               
     @info_1 =    blog.blog_posts.where(identifier: 'pro account info page headding')
                                     .first_or_create(identifier: 'pro account info page headding', 
                                                      title: 'Pro Account',
-                                                     teaser: 'Do more, Make monet')
+                                                     teaser: 'Do more, Make money')
               
     @info_2 =    blog.blog_posts.where(identifier: 'pro account info explained')
                                     .first_or_create(identifier: 'pro account info explained', 
                                                      title: "Submit opportunities",
                                                      teaser: 'not posted on the page',
-                                                     body: "Thundercats PBR pop-up, farm-to-table before they sold out four loko normcore Shoreditch. 8-bit Thundercats sartorial cred, Etsy craft beer cray vegan small batch farm-to-table aesthetic chillwave literally cardigan shabby chic. Messenger bag pickled McSweeney's, chia leggings High Life hashtag fanny pack direct trade. Viral fixie messenger bag, wayfarers gluten-free tattooed synth bitters Bushwick pour-over gastropub Wes Anderson chambray ")
+                                                     body: "Please update body ")
+              
+    @info_3 =    blog.blog_posts.where(identifier: 'pro account info explained2')
+                                    .first_or_create(identifier: 'pro account info explained', 
+                                                     title: "Submit opportunities",
+                                                     teaser: 'not posted on the page',
+                                                     body: "Please update body ")
 
+  
+  @body_color = '#000'
   end
 end
