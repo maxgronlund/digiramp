e2619aaba093a1141bec270dad0d4b2c827f84c9
Synthmax <max@synthmax.dk>
2015-05-07 15:16:23 +0200

    -

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,40 @@
 #ENV["S3_KEY_ID"]     = AMAZON_CONFIG['s3_key_id']
 #ENV["S3_ACCESS_KEY"] = AMAZON_CONFIG['s3_access_key']
 #ENV["AWS_S3_BUCKET"] = AMAZON_CONFIG['aws_s3_bucket']
+#ap '******++**********'
+#ap ENV["S3_ACCESS_REGION"]
+#
 
 
 
+#CarrierWave.configure do |config|
+#  config.fog_credentials = {
+#    provider: "AWS",
+#    aws_access_key_id:      ENV["S3_KEY_ID"] , 
+#    aws_secret_access_key:  ENV["S3_ACCESS_KEY"],
+#    region:                 ENV["S3_ACCESS_REGION"]
+#  }
+#  config.fog_directory    = Rails.application.secrets.aws_s3_bucket  #ENV["AWS_S3_BUCKET"]
+#end
 
+if Rails.env.test?
+  Rails.application.secrets.s3_key_id       = ENV["S3_KEY_ID"] 
+  Rails.application.secrets.s3_access_key   = ENV["S3_ACCESS_KEY"]
+  Rails.application.secrets.aws_s3_region   = ENV["AWS_S3_REGION"]
+  Rails.application.secrets.aws_s3_bucket   = ENV["AWS_S3_BUCKET"]
+end
 
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
     aws_access_key_id:      Rails.application.secrets.s3_key_id, # ENV["S3_KEY_ID"] , 
-    aws_secret_access_key:  Rails.application.secrets.s3_access_key, # 3ENV["S3_ACCESS_KEY"],
+    aws_secret_access_key:  Rails.application.secrets.s3_access_key, # ENV["S3_ACCESS_KEY"],
     region:                 Rails.application.secrets.aws_s3_region
   }
   config.fog_directory    = Rails.application.secrets.aws_s3_bucket  #ENV["AWS_S3_BUCKET"]
 end
+
+
 
 module CarrierWave
   module MiniMagick
