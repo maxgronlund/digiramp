7d17e66d25bbb86eb4be31402a2f3cdd571a7881
Synthmax <max@synthmax.dk>
2015-05-07 17:09:26 +0200

    removed specs/controllers

--- spec/controllers/request_works_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe RequestWorksController, :type => :controller do
-
-end
--- spec/controllers/send_messages_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SendMessagesController, :type => :controller do
-
-end
