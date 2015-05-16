1238e3e41e612d6fdf9919118583022f8fce815f
Synthmax <max@synthmax.dk>
2015-05-16 12:38:54 +0200

    -

--- db/migrate/20150516090340_create_shop_orders.rb
+++ db/migrate/20150516090340_create_shop_orders.rb
@@ -1,6 +1,6 @@
 class CreateShopOrders < ActiveRecord::Migration
   def change
-    create_table( :shop_orders, id: :uuid) do |t|
+    create_table( :shop_orders, :uuid, default: "uuid_generate_v4()") do |t|
       t.belongs_to :user, index: true, foreign_key: true
       t.belongs_to :stripe_customer, index: true, foreign_key: true
 
