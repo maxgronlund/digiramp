f55bab25982fb3a91b4a0ba7da674e4341ca2b3f
Synthmax <max@synthmax.dk>
2015-06-10 20:39:25 +0200

    -

--- db/migrate/20150609071844_add_state_to_shop_transfers.rb
+++ db/migrate/20150609071844_add_state_to_shop_transfers.rb
@@ -1,7 +1,7 @@
 class AddStateToShopTransfers < ActiveRecord::Migration
   def change
     add_column :shop_stripe_transfers, :state, :string, default: 'pending'
-    add_column :shop_stripe_transfers, :source_transaction, :string
+    #add_column :shop_stripe_transfers, :source_transaction, :string
     add_column :shop_stripe_transfers, :currency, :string
     
     
