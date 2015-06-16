12d6287bd45053bde2e8f0638da16c024f6a97c7
Synthmax <max@synthmax.dk>
2015-06-16 19:38:26 +0200

    -

--- app/controllers/api/sendgrid_hook_controller.rb
+++ app/controllers/api/sendgrid_hook_controller.rb
@@ -2,80 +2,80 @@ class Api::SendgridHookController < ApplicationController
   protect_from_forgery except: [:update]
   def update
 
-    params.each do |p|
-      
-      p.each do | params|
-        #ap params.class.name
-        if params.class.name == "ActionController::Parameters"
-          params["_json"].to_a.each do |event|
-            case event["event"]
-            when "processed"    
-              processed event
-            when "dropped"      
-              dropped event
-            when "delivered"    
-              delivered event
-            when "open"         
-              open event
-            when "click"        
-              click event
-            when "bounce"       
-              bounce event
-            when "unsubscribe"  
-              unsubscribe event
-            else
-              Opbeat.capture_message("SendgridHookController: #{event["event"]}")
-            end
-          end
-        end
-      end
-    end
+    #params.each do |p|
+    #  
+    #  p.each do | params|
+    #    #ap params.class.name
+    #    if params.class.name == "ActionController::Parameters"
+    #      params["_json"].to_a.each do |event|
+    #        case event["event"]
+    #        when "processed"    
+    #          processed event
+    #        when "dropped"      
+    #          dropped event
+    #        when "delivered"    
+    #          delivered event
+    #        when "open"         
+    #          open event
+    #        when "click"        
+    #          click event
+    #        when "bounce"       
+    #          bounce event
+    #        when "unsubscribe"  
+    #          unsubscribe event
+    #        else
+    #          Opbeat.capture_message("SendgridHookController: #{event["event"]}")
+    #        end
+    #      end
+    #    end
+    #  end
+    #end
   end
   
   
   def processed event
-    if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
-      client_invitation.processed!
-    end
+    #if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
+    #  client_invitation.processed!
+    #end
   end
   
   def dropped event
-    if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
-      client_invitation.dropped!
-      client_invitation.sendgrid_msg = event["reason"]
-      client_invitation.save
-    end
+    #if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
+    #  client_invitation.dropped!
+    #  client_invitation.sendgrid_msg = event["reason"]
+    #  client_invitation.save
+    #end
   end
   
   def delivered event
-    if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
-      client_invitation.delivered!
-    end  
+    #if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
+    #  client_invitation.delivered!
+    #end  
   end
   
   def open event
-    if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
-      client_invitation.opened!
-    end
+    #if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
+    #  client_invitation.opened!
+    #end
   end
   
   def click event
-    if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
-      client_invitation.clicked!
-    end
+    #if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
+    #  client_invitation.clicked!
+    #end
   end
   
   def bounce event
-    if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
-      client_invitation.bounced!
-      client_invitation.sendgrid_msg = event["reason"]
-      client_invitation.save
-    end
+    #if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
+    #  client_invitation.bounced!
+    #  client_invitation.sendgrid_msg = event["reason"]
+    #  client_invitation.save
+    #end
   end
   
   def unsubscribe event
-    if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
-      client_invitation.unsubscribed!
-    end
+    #if client_invitation = ClientInvitation.find_by(id: event["uniq_ids"].to_i)
+    #  client_invitation.unsubscribed!
+    #end
   end
 end
