ee2f31ccecef95c73ce22722db2671422ec67c93
Synthmax <max@synthmax.dk>
2015-06-16 17:31:17 +0200

    -

--- app/services/mandrill_account_service.rb
+++ app/services/mandrill_account_service.rb
@@ -5,18 +5,17 @@ class MandrillAccountService
     return unless user.mandrill_account_id.blank?
     begin
       
-      
       mandril_client              = Mandrill::API.new Rails.application.secrets.email_provider_password                       
       mandrill_account_id         = "cus-" + user.id.to_s 
       notes                       = "Free social account, signed up on " + user.created_at.to_formatted_s(:short)
       custom_quota                = 45
-      result                      = mandril_client.subaccounts.add mandrill_account_id, name, notes, custom_quota
+      #result                      = mandril_client.subaccounts.add mandrill_account_id, name, notes, custom_quota
       user.mandrill_account_id    = mandrill_account_id
       user.save(validate: false)
-      ap result
+      #ap result
     rescue Mandrill::Error => e
       ap "A mandrill error occurred: #{e.class} - #{e.message}"
-      Opbeat.capture_message("#{e.class} - #{e.message}")
+      #Opbeat.capture_message("#{e.class} - #{e.message}")
     end
   end
   
