ce76d0fafa71f80292194899390060a90b6daebb
Synthmax <max@synthmax.dk>
2015-06-28 00:16:53 +0200

    -

---
+++ db/migrate/20150627221558_beef_up_mandril_quora2.rb
@@ -0,0 +1,7 @@
+class BeefUpMandrilQuora2 < ActiveRecord::Migration
+  def change
+    User.find_each do |user|
+      MandrillAccountService.update_account_quota_for_user user.id
+    end
+  end
+end
