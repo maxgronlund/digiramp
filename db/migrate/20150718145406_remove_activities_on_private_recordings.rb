bf007a5036acf7c6cdb49b647ec395d0bac62a0b
Synthmax <max@synthmax.dk>
2015-07-18 17:16:59 +0200

    -

---
+++ db/migrate/20150718145406_remove_activities_on_private_recordings.rb
@@ -0,0 +1,15 @@
+class RemoveActivitiesOnPrivateRecordings < ActiveRecord::Migration
+  def change
+    count = 0
+    FollowerEvent.where(  postable_type: "Recording").each do |follower_even|
+      if rec = Recording.cached_find(follower_even.postable_id )
+        unless rec.privacy == 'Anyone'
+          follower_even.destroy
+          count += 1
+        end
+      end
+    end
+    ap '================================'
+    ap count
+  end
+end
