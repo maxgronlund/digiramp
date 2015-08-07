df5637a93748bc0c3ee634dd073a2b806df4666c
Synthmax <max@synthmax.dk>
2015-08-07 17:48:28 +0200

    -

--- app/classes/user_validator.rb
+++ app/classes/user_validator.rb
@@ -4,9 +4,12 @@ class UserValidator < ActiveModel::Validator
       record.errors[:email] << 'Email has been added to another account!'
     end
     
-    domain =  record.email.split('@').last.split('.').last(2).join(".")
-    if BlacklistDomain.where(domain: domain).first
-      record.errors[:email] << 'Email domain is blacklisted! Please contact support is you think this is an error'
+    begin
+      domain =  record.email.split('@').last.split('.').last(2).join(".")
+      if BlacklistDomain.where(domain: domain).first
+        record.errors[:email] << 'Email domain is blacklisted! Please contact support is you think this is an error'
+      end
+    rescue
     end
     #unless record.name.starts_with? 'X'
     #  record.errors[:name] << 'Need a name starting with X please!'
