85e70c20fa6794c26fb37382c0f932148c76663d
Synthmax <max@synthmax.dk>
2015-09-19 09:53:35 +0200

    -

--- db/migrate/20150917143732_setup_user_publishers.rb
+++ db/migrate/20150917143732_setup_user_publishers.rb
@@ -4,6 +4,7 @@ class SetupUserPublishers < ActiveRecord::Migration
     add_reference :documents, :belongs_to, polymorphic: true, index: true
     add_reference :publishing_agreements, :account, index: true, foreign_key: false
     add_reference :document_users, :digital_signature, index: true
+    add_reference :publishing_agreements, :user, index: true, foreign_key: false
     
     Publisher.destroy_all
     PublishingAgreement.destroy_all
