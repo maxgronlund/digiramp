19831d7a3d71d252959575b509d003b14f0e6e6d
Synthmax <max@synthmax.dk>
2015-09-24 20:50:10 +0200

    -

--- db/migrate/20150922055247_add_generated_fees_to_stakes.rb
+++ db/migrate/20150922055247_add_generated_fees_to_stakes.rb
@@ -3,8 +3,8 @@ class AddGeneratedFeesToStakes < ActiveRecord::Migration
     add_column :stakes, :generated_fee, :integer    , default: 0
     add_column :stakes, :generated_income, :integer , default: 0
     
-    Stake.find_each do |stake|
-      stake.update_income
-    end
+    #Stake.find_each do |stake|
+    #  stake.update_income
+    #end
   end
 end
