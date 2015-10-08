d8de5554feab83c3e5c7bf9db2844fb4dc75a98b
Synthmax <max@synthmax.dk>
2015-10-08 17:25:40 +0200

    -

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -14,36 +14,72 @@ class ContactMailer < ApplicationMailer
     title     = contact.title
     body      = contact.body
     #link      = url_for( controller: 'contact', action: 'index', user_id: user_id, id: issue_id)
-
+    
     begin
       template_name = "contact-request-received"
       template_content = []
+
       message = {
-        to: [{email: email, name: 'Contact request received' }],
-        from: {email: 'contact@digiramp.com'},
-        subject: title,
-        tags: ["contact", "contact-received"],
+        to: [{email: 'contact@digiramp.com' , name: 'Contact request received' }],
+        from: {email: "noreply@digiramp.com"},
+        subject: "DigiRAMP Contact request",
+        tags: ["contact", "support"],
         track_clicks: true,
         track_opens: true,
         subaccount: '09-contact-request',
-        recipient_metadata: [{rcpt: email, values: {contact_id: contact_id}}],
+        recipient_metadata: [{rcpt: 'contact@digiramp.com', values: {contact_id: 'fo'}}],
         merge_vars: [
           {
-           rcpt: email,
-           vars: [
-                   {name: "TITLE",        content: title},
-                   {name: "BODY",         content: body},
-                   {name: "SEND_FROM",    content: email}
-                  ]
+           rcpt: 'contact@digiramp.com',
+           vars: 
+           [ 
+              {name: "TITLE",        content: title},
+              {name: "BODY",         content: body},
+              {name: "SEND_FROM",    content: email}
+            ]
           }
         ]
       }
-      mandril_client.messages.send_template template_name, template_content, message
-      #Opbeat.capture_message("Contact received: #{link}")
-      SlackService.contact_request_received() #if Rails.env.production?
+      ap mandril_client.messages.send_template template_name, template_content, message
     rescue Mandrill::Error => e
-      Opbeat.capture_message("#{e.class} - #{e.message}")
-    end
+      ErrorNotification.post("#{e.class} - #{e.message}")
+    end 
+    
+    
+
+    ##begin
+    #  template_name = "contact-request-received"
+    #  template_content = []
+    #  message = {
+    #    to: [{email: 'contact@digiramp.com', name: 'Contact request received' }],
+    #    from: {email: 'noreply@digiramp.com'},
+    #    subject: title,
+    #    tags: ["contact", "contact-received"],
+    #    track_clicks: true,
+    #    track_opens: true,
+    #    #subaccount: '09-contact-request',
+    #    recipient_metadata: [{rcpt: email, values: {contact_id: contact_id}}],
+    #    merge_vars: [
+    #      {
+    #       rcpt: 'contact@digiramp.com',
+    #       vars: [
+    #               {name: "TITLE",        content: title},
+    #               {name: "BODY",         content: body},
+    #               {name: "SEND_FROM",    content: email}
+    #              ]
+    #      }
+    #    ]
+    #  }
+    #  
+    # 
+    #  
+    #  ap message
+    #  ap mandril_client.messages.send_template( template_name, template_content, message )
+    #  #Opbeat.capture_message("Contact received: #{link}")
+    #  SlackService.contact_request_received() #if Rails.env.production?
+    #  #rescue Mandrill::Error => e
+    #  #Opbeat.capture_message("#{e.class} - #{e.message}")
+      #end
   end
   
   
