f5baf12858a368a1e858fbee85ad41042b7244d0
Synthmax <max@synthmax.dk>
2015-11-15 08:53:50 +0100

    -

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -2,7 +2,7 @@
 class ShopController < ApplicationController
   def index
     
-    @user       = User.cached_find(params[:user_id])
+    return not_found unless @user       = User.cached_find(params[:user_id])
     @products   = @user.products.on_sale.where(show_in_shop: true)
     @shop_order = current_order
     #@shop_products = Shop::Product.where(for_sale: true)
