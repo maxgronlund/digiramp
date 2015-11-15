73ad39f537936767aec5f7d2b43955fe3cd157ac
Synthmax <max@synthmax.dk>
2015-11-15 09:04:28 +0100

    -

--- app/models/user_publisher.rb
+++ app/models/user_publisher.rb
@@ -8,6 +8,7 @@ class UserPublisher < ActiveRecord::Base
   after_commit :connect_to_publisher
   
   def connect_to_publisher
+    return nil if self.destroyed?
     if publisher = Publisher.find_by(email: self.email)
       self.update_columns(publisher_id: publisher.id)
     end
