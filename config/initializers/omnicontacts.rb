be9db33bc8facc5a1ec11e9d69b80d12dad046e6
Synthmax <max@synthmax.dk>
2015-11-18 18:03:55 +0100

    -

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -2,7 +2,7 @@ require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail, Rails.application.secrets.gplus_app_id, Rails.application.secrets.gplus_secret_key, {:redirect_path => "/invites/gmail/contact_callback"}
-  importer :yahoo, "dj0yJmk9TEVycmliSEo4YlhTJmQ9WVdrOWNHRnhWbkI1Tm1zbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD02ZQ--", "a3b0475b18a8864685f84192636e7cb9c3cad135", {:callback_path => '/invites/jahoo/contact_callback'} 
+  importer :yahoo, "dj0yJmk9bklXUzJxd2lydzg4JmQ9WVdrOVUwNHhlWEZDTkhFbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD05MQ--", "ae57848df503898d81baa6f4e871ad72e899bffd", {:callback_path => '/invites/yahoo/contact_callback'} 
   
   #importer :yahoo, "consumer_id", "consumer_secret", {:callback_path => '/callback'}
   #importer :linkedin, "consumer_id", "consumer_secret", {:redirect_path => "/oauth2callback", :state => '<long_unique_string_value>'}
