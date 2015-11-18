4a55bc863452dc6202b999e441f21e869e64da62
Synthmax <max@synthmax.dk>
2015-11-18 23:13:24 +0100

    -

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -27,8 +27,7 @@ class InvitesController < ApplicationController
       opens:            0
     )
     contacts.each do |c|
-      logger.info c
-      logger.info '--------------------------'
+      
       address_work  = nil
       city_work     = nil
       email_address = nil
@@ -43,77 +42,82 @@ class InvitesController < ApplicationController
               end # end if work
             end # addresses.each
           end # end if addresses
+        end
+      else
+        email_address = c[:email]
+      end
+        
+      if email_address
+        client = Client.where(
+          email:     email_address
+        )
+        .first_or_create(
+          account_id:                 current_user.account_id,
+          user_uuid:                  current_user.uuid,
+          name:                       c[:name],
+          last_name:                  c[:last_name],
+          #title:
+          #photo:
+          #telephone_home:
+          #business_phone:
+          #business_fax:
+          #fax_home:
+          #cell_phone:
+          #company:
+          #capacity:
+          #address_home:
+          address_work:            address_work,
+          city_work:               city_work,
+          #state_work:
+          #zip_work:
+          #country_work:
+          email:                   email_address,
+          #home_page:
+          #department:
+          #revision:
+          #created_at:
+          #updated_at:
+          #assistant:
+          #direct_phone:
+          #direct_fax:
+          #business_email:
+          #show_alert:
+          user_id:               current_user.id,
+          #full_name:
+          #member_id:
+          client_import_id:      @client_import.id
 
-          client = Client.where(
-            email:     email_address
+        )
+        
+        
+        
+        
+        unless client_invitation = ClientInvitation.find_by( 
+            account_id: current_user.account_id, 
+            email: client.email
           )
-          .first_or_create(
-            account_id:                 current_user.account_id,
-            user_uuid:                  current_user.uuid,
-            name:                       c[:name],
-            last_name:                  c[:last_name],
-            #title:
-            #photo:
-            #telephone_home:
-            #business_phone:
-            #business_fax:
-            #fax_home:
-            #cell_phone:
-            #company:
-            #capacity:
-            #address_home:
-            address_work:            address_work,
-            city_work:               city_work,
-            #state_work:
-            #zip_work:
-            #country_work:
-            email:                   email_address,
-            #home_page:
-            #department:
-            #revision:
-            #created_at:
-            #updated_at:
-            #assistant:
-            #direct_phone:
-            #direct_fax:
-            #business_email:
-            #show_alert:
-            user_id:               current_user.id,
-            #full_name:
-            #member_id:
-            client_import_id:      @client_import.id
+          #client_invitation = ClientInvitation.create(
+          #  account_id:   current_user.account_id, 
+          #    client_id:  client.id,
+          #    user_id:    current_user.id,
+          #    uuid:       UUIDTools::UUID.timestamp_create().to_s,
+          #    email:      client.email 
+          #)
+          
+          client_group_client = ClientGroupsClients.create(
+            client_group_id: @client_group.id,
+            client_id:       client.id,
+            user_uuid:       current_user.uuid
+          )
+          
+          #client_invitation.send_one_with_avatar
+          #invitations_send_to[invitations_send_to_index] = email 
+          #invitations_send                      += 1
+        end
+        
+       
+      end # end if email_address
 
-          )
-          
-          
-          
-          
-          unless client_invitation = ClientInvitation.find_by( 
-              account_id: current_user.account_id, 
-              email: client.email
-            )
-            #client_invitation = ClientInvitation.create(
-            #  account_id:   current_user.account_id, 
-            #    client_id:  client.id,
-            #    user_id:    current_user.id,
-            #    uuid:       UUIDTools::UUID.timestamp_create().to_s,
-            #    email:      client.email 
-            #)
-            
-            client_group_client = ClientGroupsClients.create(
-              client_group_id: @client_group.id,
-              client_id:       client.id,
-              user_uuid:       current_user.uuid
-            )
-            
-            #client_invitation.send_one_with_avatar
-            #invitations_send_to[invitations_send_to_index] = email 
-            #invitations_send                      += 1
-          end
-          
-         
-        end # end if email
-      end # end if emails
     end # end of contacts.each
     @client_group.invite_clients
 
