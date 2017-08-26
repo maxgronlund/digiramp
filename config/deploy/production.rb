add80321f16f4153300e3edecef8c60bdaf4d1bb
Max Grønlund <max@synthmax.dk>
2017-08-26 07:24:47 +0200

    cleaned config/deploy/production.rb and updated ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,18 +1,17 @@
 set :stage, :production
-set :branch, "master"
+set :branch, 'master'
 
 # This is used in the Nginx VirtualHost to specify which domains
 # the app should appear on. If you don't yet have DNS setup, you'll
 # need to create entries in your local Hosts file for testing.
-set :server_name, "www.digiramp.com digiramp.com"
-#set :server_name, "www.digiramp.com digiramp.com .digiramp.org .digiramp.net .assets-manager.com"
+set :server_name, 'www.digiramp.com digiramp.com'
 
 # used in case we're deploying multiple versions of the same
 # app side by side. Also provides quick sanity checks when looking
 # at filepaths
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
-server '213.239.210.240', user: 'deploy', roles: %w{web app db}, primary: true
+server '45.32.91.107', user: 'deploy', roles: %w[web app db], primary: true
 
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
@@ -20,14 +19,12 @@ set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 # stage name.
 set :rails_env, :production
 
-# number of unicorn workers, this will be reflected in
-# the unicorn.rb and the monit configs
-set :unicorn_worker_count, 5
+# number of @blog workers, this will be reflected in
+# the @blog.rb and the monit configs
+set :@blog_worker_count, 5
 
 # whether we're using ssl or not, used for building nginx
 # config file
 set :enable_ssl, false
 
-set :ssh_options, { :forward_agent => true }
-
-
+set :ssh_options, forward_agent: true
